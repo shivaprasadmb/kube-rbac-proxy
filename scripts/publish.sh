0f1145d315f9947c6e6204bf09be4fe5740f6d80
Stanislav Láznička <stlaz.devel@proton.me>
2026-03-23 12:39:03 +0100

    publish.sh: declare PULL_TAGS as empty if not set
    Signed-off-by: Stanislav Láznička <slznika@microsoft.com>

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,18 +8,19 @@ set -u
 # for debugging
 set -x
 
-if [[ -n "$PULL_TAGS" ]]; then
-  # github actions, by default, fetches using `--no-tags`.
-  # we need tags though to create a release version string.
-  git fetch --tags --force
-fi
-
 QUAY_PATH="${QUAY_PATH:-quay.io/brancz/kube-rbac-proxy}"
 CPU_ARCHS="amd64 arm64 arm ppc64le s390x"
 TAG_COMMIT=$(git rev-list --abbrev-commit --tags --max-count=1)
 COMMIT=$(git rev-parse --short HEAD)
 TAG=$(git describe --abbrev=0 --tags ${TAG_COMMIT})
 VERSION="${TAG}"
+PULL_TAGS=${PULL_TAGS:-""}
+
+if [[ -n "$PULL_TAGS" ]]; then
+  # github actions, by default, fetches using `--no-tags`.
+  # we need tags though to create a release version string.
+  git fetch --tags --force
+fi
 
 # if the current commit, does not correspond to a tag, create a verbose version string.
 if [ "${TAG_COMMIT}" != "${COMMIT}" ]; then
